965234dea8ca5ef6fdf2f8fc4a9672e7c9b7f4b5
Mehmet Hanaylı <mehmet@adu.edu.tr>
2022-10-27 16:24:19 +0300

    Ders-2 için ödev eklendi.

---
+++ Ders-2-odev/odev.php
@@ -0,0 +1,24 @@
+<?php
+
+/* 
+Uygulama-1
+
+1. Kendi belirleyeceğiniz isimde bir sınıf oluşturunuz.
+2. constract metodunda 3 tane değişken alarak bu değişkenleri sınıf içerisindeki değişkenlere atayınız.
+3. sınıf içerisine public 2 tane metot yazınız.
+4. Birinci metotta  girilen sayının karesi ekrana yazdırılsın.
+5. İkinci metotta  girilen sayıya kadar tüm sayıların karesi alt alta ekrana yazdırılsın.
+
+Uygulama-2
+
+1. Kendi belirleyeceğiniz isimde bir sınıf oluşturunuz.
+2. Construct metodunda bir karşılama mesajını ekrana yazdırınız.
+3. Destruct metodunu yazınız ve çalıştığını gösteren bir metni ekrana yazdırınız.
+4. sınıf içerisine public 2 tane metot yazınız.
+5. Birinci metoda parametre olarak gönderilen vize ve final notlarını alarak, ortalama hesabı yaptırıp 50'nin üzerindeyse geçtiğini ekrana yazdırınız.
+6. İkinci metotda  parametre olarak gelen sayının faktöryel hesabını yaptırarak ekrana sonucunu yazdırınız. 
+
+*/
+
+
+?>
