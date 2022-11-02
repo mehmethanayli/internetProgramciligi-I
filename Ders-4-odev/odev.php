cfa48ec180f3abdb53bcb1b074b85617ae31999f
Mehmet Hanaylı <mehmet@adu.edu.tr>
2022-11-02 19:22:06 +0300

    Ders 4 uyglaması ve ödevi tamamlandı.

---
+++ Ders-4-odev/odev.php
@@ -0,0 +1,20 @@
+<?php
+
+/* 
+Uygulama-1
+
+1. Kendi belirleyeceğiniz isimde bir sınıf oluşturunuz.
+2. constract metodunda 3 tane değişken alarak bu değişkenleri sınıf içerisindeki değişkenlere atayınız.
+3. sınıf içerisine public 2 tane metot yazınız.
+4. Oluşturmuş olduğunuz sınıfı miras alan 2 yeni sınıf yazarak metot overriding işlemini örneklendiriniz.
+
+Uygulama-2
+
+1. Kendi belirleyeceğiniz isimde bir sınıf oluşturunuz.
+2. Construct metodunda bir karşılama mesajını ekrana yazdırınız.
+3. sınıf içerisine  1 tane final metot yazınız.
+4. Yazmış olduğunuz sınıfı miras alan iki yeni sınıf yazınız. Yazılımış sınıflardan bir tanesini final olarak belirleyiniz. 
+5. Miras alan sınıflarda final metodun nasıl kullanıldığını ve override işlemini örnekle açıklayınız.
+6. Final sınıfı miras alan yeni bir sınıf oluştunuz. Son durumu açıklayınız.
+
+*/
